8eb57c6dd9d690424039d550846c752797439f0e
Noah Böhringer <noah.boehri29@gmail.com>
2022-11-06 18:22:37 +0100

    changed app name

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("./dist/twofootball-app"));
+app.use(express.static("./dist/two-football-app"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/twofootball-app/" })
+  res.sendFile("index.html", { root: "dist/two-football-app/" })
 );
 
 app.listen(process.env.PORT || 8080);
