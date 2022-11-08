fb86f0309990337f937294545f4a398623deeed5
Noah Böhringer <noah.boehri29@gmail.com>
2022-11-08 11:56:50 +0100

    Checked bugs

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
-
 app.use(express.static("./dist/two-football-app"));
-
 app.get("/*", (req, res) =>
   res.sendFile("index.html", { root: "dist/two-football-app/" })
 );
-
 app.listen(process.env.PORT || 8080);
